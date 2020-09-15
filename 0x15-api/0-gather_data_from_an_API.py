eb0ddc356a111a23321ca762ca68c8448777ac56
fredhii <fredhiixd@gmail.com>
2020-09-14 19:12:28 -0500

    add first point

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Calls an API in order to get completed tasks """
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    userId = argv[1]
+    userData = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                            format(userId), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId), verify=False).json()
+    completed_tasks = []
+    for task in todo:
+        if task.get('completed') is True:
+            completed_tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(userData.get('name'), len(completed_tasks), len(todo)))
+    print("\n".join("\t {}".format(task) for task in completed_tasks))
