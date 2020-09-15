6c4f0e66b4f95e21086faead68a1746d2814f13e
fredhii <fredhiixd@gmail.com>
2020-09-14 19:48:11 -0500

    fix #2

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """ Calls an API in order to get completed tasks """
 import requests
-import urllib3
 from sys import argv
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 if __name__ == '__main__':
-    userId = argv[1]
-    userData = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                            format(userId), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId), verify=False).json()
-    completed_tasks = []
-    for task in todo:
-        if task.get('completed') is True:
-            completed_tasks.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):".
-          format(userData.get('name'), len(completed_tasks), len(todo)))
-    print("\n".join("\t {}".format(task) for task in completed_tasks))
+    try:
+        userId = argv[1]
+        userData = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}".
+            format(userId), verify=False).json()
+        toDo = requests.get(
+            "https://jsonplaceholder.typicode.com/todos?userId={}".
+            format(userId), verify=False).json()
+        completedTasks = []
+        for task in toDo:
+            if task.get('completed') is True:
+                completedTasks.append(task.get('title'))
+        print("Employee {} is done with tasks({}/{}):".
+              format(userData.get('name'), len(completedTasks), len(toDo)))
+        for task in completedTasks:
+            print('\t {}'.format(task))
+    except ValueError:
+        exit
