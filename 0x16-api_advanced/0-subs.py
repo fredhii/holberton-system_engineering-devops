c13f2f8f12f8cacbf6ddb7485495392bf41cff4c
fredhii <fredhiixd@gmail.com>
2020-09-17 17:59:36 -0500

    add API project

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Contains the number_of_subscribers function """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+        Returns the number of subscribers to a specified subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    user_agent = {'User-Agent': 'Fredhii'}
+    req = requests.get(url, headers=user_agent, allow_redirects=False)
+    if req.status_code == 200:
+        req = req.json()
+        data = req.get('data')
+        subscribers = data.get('subscribers')
+        if data is not None and subscribers is not None:
+            return subscribers
+    return 0
